c8f491f5d45d68f310f5c0b7a78ace05aac46d04
bugfroggy <bugfroggy@gmail.com>
2018-08-17 22:52:46 -0400

    * Added more elements * Fixed issue where elements already in the DOM before script load weren't changing color if they weren't updated * Simplified code a bit, added some comments

--- chrome/init.js
+++ chrome/init.js
@@ -1,46 +1,41 @@
 // Apply CSS
 updateStyle();
 
+function checkNode(node) {
+    if (node instanceof HTMLElement)
+        if(!node.hasAttribute("data-gmusic-color-props")) {
+            // Not registered, check if needs to be registered
+            for (let i = 0; i < targets.length; i++) {
+                let target = targets[i];
+                if (node.matches(target.name)) { // Matches a target, register it
+                    node.setAttribute("data-gmusic-color-props", target.properties.join(" "));
+                    node.setAttribute("data-gmusic-color-selector", target.name);
+                    break;
+                }
+            }
+        } else {
+            // Already registered, check if needs to be unregistered
+            if(!node.matches(node.getAttribute("data-gmusic-color-selector"))) // No longer matches its target
+                node.removeAttribute("data-gmusic-color-props");
+        }
+}
+
+// Check all current elements of DOM at init
+let selector;
+for(let i = 0; i < targets.length; i++)
+    selector += targets[i].name + ",";
+selector = selector.substring(0, selector.length - 1);
+document.querySelectorAll(selector).forEach(checkNode);
+
 // Listen for changes to the DOM
 new MutationObserver((mutations) => {
     mutations.forEach((data) => {
         if(data.type === "attributes") {
-            if(data.target instanceof HTMLElement) {
-                if(!data.target.hasAttribute("data-gmusic-props")) {
-                    // Not registered, check if needs to be registered
-                    for (let i = 0; i < targets.length; i++) {
-                        let target = targets[i];
-                        if (data.target.matches(target.name)) { // Matches a target, register it
-                            data.target.setAttribute("data-gmusic-props", target.properties.join(" "));
-                            data.target.setAttribute("data-gmusic-color-selector", target.name);
-                            break;
-                        }
-                    }
-                } else {
-                    // Already registered, check if needs to be unregistered
-                    if(!data.target.matches(data.target.getAttribute("data-gmusic-color-selector"))) // No longer matches its target
-                        data.target.removeAttribute("data-gmusic-props");
-                }
-            }
+            checkNode(data.target);
         } else if(data.type === "childList") {
             if (data.addedNodes != null && data.addedNodes.length > 0) {
                 data.addedNodes.forEach((node) => {
-                   if (node instanceof HTMLElement)
-                       if(!node.hasAttribute("data-gmusic-props")) {
-                           // Not registered, check if needs to be registered
-                           for (let i = 0; i < targets.length; i++) {
-                               let target = targets[i];
-                               if (node.matches(target.name)) { // Matches a target, register it
-                                   node.setAttribute("data-gmusic-props", target.properties.join(" "));
-                                   node.setAttribute("data-gmusic-color-selector", target.name);
-                                   break;
-                               }
-                           }
-                       } else {
-                           // Already registered, check if needs to be unregistered
-                           if(!node.matches(node.getAttribute("data-gmusic-color-selector"))) // No longer matches its target
-                               node.removeAttribute("data-gmusic-props");
-                       }
+                  checkNode(node);
                 })
             }
         }
--- chrome/target_data.js
+++ chrome/target_data.js
@@ -18,35 +18,92 @@ let targets = [ // TODO fix bug with this producing an error saying targets is a
         // I'm feeling lucky button
         name: ".paper-fab-0",
         properties: ["background"]
+    },
+    {
+        name: ".paper-spinner-lite-2 .spinner-layer.paper-spinner-lite",
+        properties: ["color"]
+    },
+    {
+        name: "iron-icon.primary:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: "#playlist-drawer:not([style-scope]):not(.style-scope) .playlist-drawer-item.playlist-drop-target:not([style-scope]):not(.style-scope) .playlist-wrapper:not([style-scope]):not(.style-scope) .playlist-title:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: ".nav-item-container.selected:not([style-scope]):not(.style-scope),.nav-item-container.selected:not([style-scope]):not(.style-scope) iron-icon:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: "#player:not([style-scope]):not(.style-scope) paper-icon-button[data-id=\"repeat\"].active:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) paper-icon-button[data-id=\"shuffle\"].active:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) #exp-menu-shuffle.active:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) #exp-menu-repeat.active:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) #exp-menu-queue.opened:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) paper-icon-button[data-id=\"queue\"].opened:not([style-scope]):not(.style-scope),#player:not([style-scope]):not(.style-scope) paper-icon-button:not([style-scope]):not(.style-scope) #exp-menu-queue.opened:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: "#player:not([style-scope]):not(.style-scope) paper-icon-button[data-id=\"play-pause\"]:not([style-scope]):not(.style-scope):not([disabled])",
+        properties: ["color"]
+    },
+    {
+        name: ".primary:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: ".primary-background:not([style-scope]):not(.style-scope)",
+        properties: ["background-color"]
+    },
+    {
+        name: "paper-icon-button.sj-callout-target:not([style-scope]):not(.style-scope)",
+        properties: ["background-color"]
+    },
+    {
+        name: ".top-charts-view:not([style-scope]):not(.style-scope) .song-row:not([style-scope]):not(.style-scope) [data-col=\"index\"]:not([style-scope]):not(.style-scope) .column-content:not([style-scope]):not(.style-scope),paper-toast:not([style-scope]):not(.style-scope) paper-button[data-action=\"button\"]:not([style-scope]):not(.style-scope),.ups.light:not([style-scope]):not(.style-scope) iron-icon:not([style-scope]):not(.style-scope),.upload-dialog-dragover:not([style-scope]):not(.style-scope) .upload-dialog-description:not([style-scope]):not(.style-scope),paper-dialog:not([style-scope]):not(.style-scope) .buttons:not([style-scope]):not(.style-scope) paper-button:not([style-scope]):not(.style-scope):not([disabled])",
+        properties: ["color"]
+    },
+    {
+        name: ".goog-menu:not([style-scope]):not(.style-scope) .goog-menuitem.selected:not([style-scope]):not(.style-scope) .goog-menuitem-content:not([style-scope]):not(.style-scope)",
+        properties: ["color"]
+    },
+    {
+        name: ".upload-dialog-title:not([style-scope]):not(.style-scope)",
+        properties: ["background-color"]
+    },
+    {
+        name: ".upload-dialog-dragover:not([style-scope]):not(.style-scope)",
+        properties: ["border-color"]
     }
 ];
 
 function updateStyle() {
-    console.log("Test!");
-
+    // Get color
     chrome.storage.sync.get(["gmusic_color"], function(items) {
         let color = items.gmusic_color;
 
+        // Delete previously created gmusic-color style if its there
         let currentStyleElem = document.getElementById("gmusic-color-style");
         if(currentStyleElem != null)
             currentStyleElem.parentElement.removeChild(currentStyleElem);
 
-        let head = document.getElementsByTagName("head")[0];
+        // Create a new style
         let style = document.createElement("style");
         style.id = "gmusic-color-style";
-
         style.type = 'text/css';
 
-        // Play button
-        style.appendChild(document.createTextNode("#player:not([style-scope]):not(.style-scope) paper-icon-button[data-id=\"play-pause\"]:not([style-scope]):not(.style-scope):not([disabled]){ color:" + color + " !important;}"));
-        // Progress bar
-        style.appendChild(document.createTextNode(".paper-progress-2 #primaryProgress.paper-progress { background: " + color + " !important; }"));
-        // Progress bar slider
-        style.appendChild(document.createTextNode(".paper-slider-0 .slider-knob-inner.paper-slider { background-color: " + color + " !important; border-color: " + color + " !important;}"));
-        // "I'm feeling lucky" button
-        style.appendChild(document.createTextNode(".paper-fab-0 { background: " + color + " !important;"))
+        // Keep track of properties already set in the for loop
+        let setProperties = [];
+        // Append all of the style properties to the new style element
+        for(let i = 0; i < targets.length; i++) {
+            for(let j = 0; j < targets[i].properties.length; j++) {
+                let property = targets[i].properties[j];
+                if(!setProperties.includes(property)) {
+                    let stylePropStr = "*[data-gmusic-color-props~=\"" + property + "\"] { " + property + ": " + color + " !important; }";
+                    style.appendChild(document.createTextNode(stylePropStr));
+                    setProperties.push(property);
+                }
+            }
+        }
 
-        head.appendChild(style);
+        // Append style to head
+        document.getElementsByTagName("head")[0].appendChild(style);
     });
 
 }
